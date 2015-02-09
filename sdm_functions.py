c42c8a464f89f99a3309b837f8e1e527ff130f8c
Toby Wise <tobyw31@gmail.com>
2015-02-09 15:44:29 +0000

    Fixed subprocess calls with shlex function for multiple arguments

--- sdm_functions.py
+++ sdm_functions.py
@@ -3,11 +3,12 @@ from bs4 import BeautifulSoup
 import re
 import subprocess
 import os
+import shlex
 
 # ############################################################################################################
-# ## This script takes the SDM cluster results in HTML format and extracts values for the peak coordinates  ##
-# ## Function 1 (get_coords) produces a list of coordinates from the HTML file                              ##
-# ## Function 2 (extract_coordinate_values) takes a list of coordinates and extracts values at these points ##
+# ## These functions take SDM cluster results in HTML format and extracts values for the peak coordinates   ##
+# ## get_coords: produces a list of coordinates from the HTML file                                          ##
+# ## extract_coordinate_values: takes a list of coordinates and extracts values at these points             ##
 # ## SDM will output the results in the current working directory - make sure to set this before running    ##
 # ## Extracted files are numbered - need to make a note of which number corresponds to each cluster         ##
 # ############################################################################################################
@@ -55,28 +56,54 @@ def extract_coordinate_values(coordinates, prefix, sdm_path):
     Prints each cluster name and coordinates when run
     """
 
+    sdm_path += " "
     for i in range(len(coordinates)):
         name = prefix + "_coords_" + str(i+1)  # format = prefix_coords_#
         mask_arg = name + " = mask coordinate " + coordinates[i]  # arguments for masking function
         extract_arg = "extract " + name  # arguments for extraction function
         print mask_arg  # prints mask arguments to make sure they're correct
-        subprocess.call([sdm_path, mask_arg])
-        subprocess.call([sdm_path, extract_arg])
+        subprocess.call(shlex.split(sdm_path + mask_arg))
+        subprocess.call(shlex.split(sdm_path + extract_arg))
 
 
 # Example usage
-"""
-result_coords = get_coords('C:/Users/Tobyyy/Dropbox/PhD/DTI/dti_ma/MDD Only/14_03_14_MDD_Meds_1m0_z_p0.00050_1.000_10.htm')
+
+result_coords = get_coords('C:/Users/k1327409/Dropbox/PhD/DTI/dti_ma/MDD Only/14_03_14_MDD_Meds_1m0_z_p0.00050_1.000_10.htm')
 extract_coordinate_values(result_coords, "BD_mean", "C:/Users/k1327409/Dropbox/PhD/Things/sdm_v4.12/sdm_v4.12/sdm.bat")
 
+
+# ############################################################################################################
+# ## This function thresholds all jackknife results in a given directory                                    ##
+# ############################################################################################################
+
+
+def threshold_jackknife(directory, sdm_path):
+    """
+    Looks for jackknife results in a given directory and thresholds them all
+    Uses default settings (p < .005, peak height = 1, extent = 10
+
+    Arguments
+    ---------
+    directory: Directory where the jackknife results are
+    sdm_path: Path to SDM (must be to the SDM.bat file)
+    """
+
+    sdm_path += " "
+    files = os.listdir(directory)
+    results = []
+    for file in files:
+        if re.match(r'.+JK.+(?<!QH)_z.htm', file):
+            results.append(file.replace('.htm', ''))
+
+    for result in results:
+        arg = 'threshold ' + result + ', p, 0.005, 1, 10'
+        print arg
+        subprocess.call(shlex.split(sdm_path + arg))
+
+# Example usage
 """
-
-# ############################################################################################################
-# ## This function thresholds jackknife results                                                             ##
-# ############################################################################################################
-
-
-
+threshold_jackknife('C:/Users/k1327409/Documents/MA stuff', 'C:/Users/k1327409/Dropbox/PhD/Things/sdm_v4.12/sdm_v4.12/sdm.bat')
+"""
 
 
 # ############################################################################################################
